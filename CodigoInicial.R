c940d9c5bef21743be5b946a0515f849d7218c7f
MarlonJa <marlonja@hotmail.com>
2019-01-11 14:53:28 -0500

    Agragamos una gráfica con ggplot2.

--- CodigoInicial.R
+++ CodigoInicial.R
@@ -1,7 +1,13 @@
 library(tidyverse)
 
 datos <- table1
+datos$year <- as.numeric(datos$year)
 
 datos %>%
   mutate(prom = population / cases) %>% 
-  arrange(prom)
+  arrange(prom) %>% 
+  
+  #ejemplo de una gráfica de puntos, dando color por año
+  ggplot(aes(country, prom, color = factor(year))) + 
+  geom_point()
+
